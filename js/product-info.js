cd165073c19caf33d4401b23ccad91ba4b427c16
santimonti <santimonti1999@gmail.com>
2020-09-03 10:26:37 -0300

    Prodcut info

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,91 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+document.addEventListener("DOMContentLoaded", function (t) {
+  getJSONData(PRODUCT_INFO_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      productInfo = resultObj.data;
+
+      showProductInfo();
+    }
+  });
+});
+function showProductInfo() {
+  let htmlContentToAppend = "";
+
+  htmlContentToAppend += `
+  
+  <div class="slideshow-container">
+
+  <!-- Full-width images with number and caption text -->
+  <div class="mySlides fade">
+    <div class="numbertext">1 / 3</div>
+    <img src="/img/prod1_1.jpg" style="width:100%">
+    <div class="text">Caption Text</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">2 / 3</div>
+    <img src="/img/prod1_2.jpg" style="width:100%">
+    <div class="text">Caption Two</div>
+  </div>
+
+  <div class="mySlides fade">
+    <div class="numbertext">3 / 3</div>
+    <img src="/img/prod1_3.jpg" style="width:100%">
+    <div class="text">Caption Three</div>
+  </div>
+
+  <!-- Next and previous buttons -->
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<!-- The dots/circles -->
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>`;
+  document.getElementById("prodInner").innerHTML = htmlContentToAppend;
+}
+
 document.addEventListener("DOMContentLoaded", function (e) {});
-document.addEventListener("DOMContentLoaded", function (e) {});
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  console.log(n);
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  console.log(slides);
+  var dots = document.getElementsByClassName("dot");
+  console.log(slides.length);
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  console.log(slides[slideIndex - 1]);
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
